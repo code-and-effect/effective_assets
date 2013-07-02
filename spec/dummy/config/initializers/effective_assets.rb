ae0231d8b6cec7978dac437d1e543e15d3234ac8
Matt Riemer <matthew@agilestyle.com>
2013-07-02 15:29:50 -0600

    Setting up dummy app for testing

---
+++ spec/dummy/config/initializers/effective_assets.rb
@@ -0,0 +1,20 @@
+# EffectiveAssets Rails Engine
+
+EffectiveAssets.setup do |config|
+  config.assets_table_name = :assets
+  config.attachments_table_name = :attachments
+
+  config.uploader = AssetUploader
+
+  # This is your S3 bucket information
+  config.aws_bucket = ''
+  config.aws_access_key_id = ''
+  config.aws_secret_access_key = ''
+
+  config.aws_final_path = 'assets/'
+  config.aws_upload_path = 'uploads/'
+  config.aws_acl = 'public-read'
+
+  config.authorization_method = Proc.new { |controller, action, resource| true }
+end
+
--- spec/models/asset_spec.rb
+++ spec/models/asset_spec.rb
@@ -9,38 +9,38 @@ describe Effective::Asset do
   end
 end
 
-# describe Asset do
-#   let(:image_url) { 'http://cdn.sstatic.net/stackoverflow/img/sprites.png?v=1' }
+describe Effective::Asset do
+  let(:image_url) { 'http://cdn.sstatic.net/stackoverflow/img/sprites.png?v=1' }
 
-#   it 'should be creatable from URL' do
-#     asset = Asset.create_from_url(image_url, {:title => 'a title', :description => 'a description', :tags => 'a tags', :user_id => 1})
+  it 'should be creatable from URL' do
+    asset = Effective::Asset.create_from_url(image_url, {:title => 'a title', :description => 'a description', :tags => 'a tags', :user_id => 1})
 
-#     # A new asset should exist, and it should be unprocessed
-#     asset.upload_file.should eq image_url
-#     asset.processed.should eq false
+    # A new asset should exist, and it should be unprocessed
+    asset.upload_file.should eq image_url
+    asset.processed.should eq false
 
-#     # It should have queued up a process_asset task with delayed job
-#     Delayed::Job.count.should eq 1
+    # It should have queued up a process_asset task with delayed job
+    Delayed::Job.count.should eq 1
 
-#     job = Psych.load(Delayed::Job.first.handler)
-#     job.method_name.should eq :process_asset_without_delay
-#     job.args.first.should eq asset
+    job = Psych.load(Delayed::Job.first.handler)
+    job.method_name.should eq :process_asset_without_delay
+    job.args.first.should eq asset
 
-#     # Run DelayedJob
-#     Delayed::Worker.new(:max_priority => nil, :min_priority => nil, :quiet => true).work_off
-#     Delayed::Job.count.should eq 0
+    # Run DelayedJob
+    Delayed::Worker.new(:max_priority => nil, :min_priority => nil, :quiet => true).work_off
+    Delayed::Job.count.should eq 0
 
-#     # We should have a totally processed Asset
-#     asset = Asset.find(asset.id)
-#     asset.processed.should eq true
-#     asset.data.kind_of?(AssetUploader).should eq true
-#     asset.title.should eq 'a title'
-#     asset.description.should eq 'a description'
-#     asset.tags.should eq 'a tags'
-#     asset.user_id.should eq 1
-#     asset.versions_info.present?.should eq true
-#     asset.content_type.should eq 'image/png'
-#     asset.height.should eq 1073
-#     asset.width.should eq 238
-#   end
-# end
+    # We should have a totally processed Asset
+    asset = Effective::Asset.find(asset.id)
+    asset.processed.should eq true
+    asset.data.kind_of?(AssetUploader).should eq true
+    asset.title.should eq 'a title'
+    asset.description.should eq 'a description'
+    asset.tags.should eq 'a tags'
+    asset.user_id.should eq 1
+    asset.versions_info.present?.should eq true
+    asset.content_type.should eq 'image/png'
+    asset.height.should eq 1073
+    asset.width.should eq 238
+  end
+end
