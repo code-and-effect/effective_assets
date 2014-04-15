8943f2acb38632a9791456de26d97d5a3e25e137
Matt Riemer <matthew@agilestyle.com>
2014-04-15 14:08:50 -0600

    Effective Asset as an inline snippet

--- app/models/effective/snippets/effective_asset.rb
+++ app/models/effective/snippets/effective_asset.rb
@@ -3,6 +3,15 @@ module Effective
     class EffectiveAsset < Snippet
       attribute :asset_id, Integer
       attribute :html_class, String
+
+      def asset
+        @asset ||= (Effective::Asset.where(:id => asset_id).first if asset_id)
+      end
+
+      def snippet_inline
+        true
+      end
+
     end
   end
 end
