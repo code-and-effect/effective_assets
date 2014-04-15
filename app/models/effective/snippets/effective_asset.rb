13529dfcd810172567556cf21fd006c716eff35c
Matt Riemer <matthew@agilestyle.com>
2014-04-15 16:43:43 -0600

    Don’t rely on Effective regions

--- app/models/effective/snippets/effective_asset.rb
+++ app/models/effective/snippets/effective_asset.rb
@@ -1,17 +1,19 @@
-module Effective
-  module Snippets
-    class EffectiveAsset < Snippet
-      attribute :asset_id, Integer
-      attribute :html_class, String
+if defined?(Effective::Snippets::Snippet)
+  module Effective
+    module Snippets
+      class EffectiveAsset < Snippet
+        attribute :asset_id, Integer
+        attribute :html_class, String
 
-      def asset
-        @asset ||= (Effective::Asset.where(:id => asset_id).first if asset_id)
+        def asset
+          @asset ||= (Effective::Asset.where(:id => asset_id).first if asset_id)
+        end
+
+        def snippet_inline
+          true
+        end
+
       end
-
-      def snippet_inline
-        true
-      end
-
     end
   end
 end
