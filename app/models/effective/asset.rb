7ea3e8397e32fb017aa838422065a177e2b16e1c
Matt Riemer <matthew@codeandeffect.com>
2023-11-29 10:46:35 -0700

    oldschool serialize

--- app/models/effective/asset.rb
+++ app/models/effective/asset.rb
@@ -46,13 +46,8 @@ module Effective
     validates :height, numericality: { allow_nil: true }
     validates :width, numericality: { allow_nil: true }
 
-    if EffectiveResources.serialize_with_coder?
-      serialize :versions_info, type: Hash, coder: YAML
-      serialize :extra, type: Hash, coder: YAML
-    else
-      serialize :versions_info, Hash
-      serialize :extra, Hash
-    end
+    serialize :versions_info, Hash
+    serialize :extra, Hash
 
     before_validation :set_content_type
     before_save :update_asset_dimensions
